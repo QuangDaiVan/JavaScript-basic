ee75683053cff783faeb8cce697aa830549626d2
caloc <quangdv2802@gmail.com>
2024-08-17 10:34:42 +0700

    js lession 10

---
+++ JS-10/array.js
@@ -0,0 +1,11 @@
+// trong js, amgr (array) là 1 kiểu dữ liệu dùng để lưu trữ danh sách các giá trị.
+// các giá trị trong mảng có thể là bất kỳ kiểu dữ liệu nào, number, string, object, thậm chí là các mảng khác.
+// mảng trong js có các chỉ số - index bắt đầu từ 0, nghĩa là phần tử đầu tiên của mảng có index là 0, phần tử thứ 2 có index là 1, và cứ tiếp tục tăng dần
+
+const arr = ['a', 'b', { a: 1, b: 2 }]
+console.log(arr)
+
+// truy cập phần tử của mảng theo index
+
+// mảng có thể chứa bất kỳ kiểu dữ liệu nào, ví dụ:
+
